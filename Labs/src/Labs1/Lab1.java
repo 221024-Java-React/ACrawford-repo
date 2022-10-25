ecfaa4ad7bf436818e6fb85d842d59d7710b2d20
Andrew Crawford <crawf398@gmail.com>
2022-10-25 12:40:54 -0500

    Add lab 1

---
+++ Labs/src/Labs1/Lab1.java
@@ -0,0 +1,22 @@
+package Labs1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        boolean boo = true;
+        byte maxByte = 127;
+        short minShort = -32_768;
+        int maxInt = 2_147_483_647;
+        long javaFlex = 3_000_000_000L;
+        float pie = 3.14159f;
+        double doubleYum = 3.14159 * 2;
+        
+        System.out.println("Boolean:" + boo);
+        System.out.println("Byte:" + maxByte);
+        System.out.println("Short: " + minShort);
+        System.out.println("Int:" + maxInt);
+        System.out.println("Long:" + javaFlex);
+        System.out.println("Float:" + pie);
+        System.out.println("Double:" + doubleYum);
+    }
+}
+
